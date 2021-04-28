92486b4d9d6e036488225fbd2d4ac851caa0e1cf
ibrahimhuycn <ibrahim.hucyn@live.com>
2021-04-28 20:06:18 +0500

    include grater than flag  in the results

--- SwatInc.Lis.Lis02A2/ResultRecord.cs
+++ SwatInc.Lis.Lis02A2/ResultRecord.cs
@@ -3,50 +3,58 @@ using System.Runtime.CompilerServices;
 
 namespace SwatInc.Lis.Lis02A2
 {
-	public class ResultRecord : AbstractLisRecord
-	{
+    public class ResultRecord : AbstractLisRecord
+    {
+        private string data;
 
-		[LisRecordField(2)]
-		public int SequenceNumber { get; set; }
+        [LisRecordField(2)]
+        public int SequenceNumber { get; set; }
 
-		[LisRecordField(3)]
-		public UniversalTestID UniversalTestID { get; set; }
+        [LisRecordField(3)]
+        public UniversalTestID UniversalTestID { get; set; }
 
-		[LisRecordField(4)]
-		public string Data { get; set; }
+        [LisRecordField(4)]
+        public string Data 
+        {
+            get 
+            {
+                if (ResultAbnormalFlag == ResultAbnormalFlags.AboveAbsoluteHigh) { return $">{data}"; }
+                return data;
+            } 
+            set => data = value; }
 
-		[LisRecordField(5)]
-		public string Units { get; set; }
+        [LisRecordField(5)]
+        public string Units { get; set; }
 
-		[LisRecordField(7)]
-		public ResultAbnormalFlags ResultAbnormalFlag { get; set; }
+        [LisRecordField(7)]
+        public ResultAbnormalFlags ResultAbnormalFlag { get; set; }
 
-		[LisRecordField(9)]
-		public ResultStatus ResultStatus { get; set; }
+        [LisRecordField(9)]
+        public ResultStatus ResultStatus { get; set; }
 
-		[LisRecordField(11)]
+        [LisRecordField(11)]
         public string OperatorIdentification { get; set; }
 
-		[LisDateTimeUsage(LisDateTimeUsage.DateTime)]
-		[LisRecordField(12)]
-        public DateTime? TestStartedDateTime{ get; set; }
+        [LisDateTimeUsage(LisDateTimeUsage.DateTime)]
+        [LisRecordField(12)]
+        public DateTime? TestStartedDateTime { get; set; }
 
-		[LisDateTimeUsage(LisDateTimeUsage.DateTime)]
-		[LisRecordField(13)]
-		public DateTime? TestCompletedDateTime { get; set; }
+        [LisDateTimeUsage(LisDateTimeUsage.DateTime)]
+        [LisRecordField(13)]
+        public DateTime? TestCompletedDateTime { get; set; }
 
-		public override string ToLISString()
-		{
-			return "R" + new string(LISDelimiters.FieldDelimiter, 1) + base.ToLISString();
-		}
+        public override string ToLISString()
+        {
+            return "R" + new string(LISDelimiters.FieldDelimiter, 1) + base.ToLISString();
+        }
 
-		public ResultRecord(string aLisString)
-			: base(aLisString)
-		{
-		}
+        public ResultRecord(string aLisString)
+            : base(aLisString)
+        {
+        }
 
-		public ResultRecord()
-		{
-		}
-	}
+        public ResultRecord()
+        {
+        }
+    }
 }
