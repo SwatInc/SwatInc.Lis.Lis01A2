2fe06e8da566c50f7810634493e0063bcb668592
Olivier Schwab <olivier.schwab@quantamatrix.com>
2023-12-20 11:01:58 +0100

    Added Comment record

---
+++ SwatInc.Lis.Lis02A2/CommentRecord.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwatInc.Lis.Lis02A2
+{
+    public class CommentRecord : AbstractLisRecord
+    {
+        [LisRecordField(4)]
+        public string Text { get; set; }
+        [LisRecordField(5)]
+        public string Type { get; set; }
+
+        public override string ToLISString()
+        {
+            return "C" + new string(LISDelimiters.FieldDelimiter, 1) + base.ToLISString();
+        }
+
+        public CommentRecord(string aLisString)
+            : base(aLisString)
+        {
+        }
+
+        public CommentRecord()
+        {
+        }
+    }
+}
